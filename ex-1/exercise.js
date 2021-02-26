d7285cca98fc5355223c5c8e717aec8d5b60203f
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-26 14:18:25 +0100

    exercise

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -5,11 +5,11 @@
 
  function myFunction() {
     
-    var noun = prompt("Welcome please a noun");
-    var verb = prompt("please enter a verb" );
-    var adverb = prompt("please enter an adverb! ");
+    var noun = prompt(" Welcome please a noun ");
+    var verb = prompt(" please enter a verb " );
+    var adverb = prompt(" please enter an adverb! ");
     var adjective = prompt(" please enter an adjective! ");
-    alert(noun + verb + adverb +adjective )
+    alert(" i love " + noun + "to " + verb + adverb+ adjective);
 
 }
-  myFunction() 
+  myFunction(); 
