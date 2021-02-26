bc06fcfca45c9eaed5f7d35585c69ec91779211a
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-02-26 14:57:31 +0100

    exercise2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,23 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+var children = prompt("please number of chldren");
+var partner = prompt("please your partners name");
+var place = prompt("please your city");
+var job = prompt("please your job title")
+
+function tellFortune(children, partner, place, job) {
+    this.children = children
+    this.partner = partner
+    this.place = place
+    this.job = job
+    this.fortune = function() {
+        alert(" you are going to be married to " + partner + " live in " + place + " work as " + job + " with " + children + " kids ")
+    }
+}
+var stanley = new tellFortune(children, partner, place, job);
+stanley.fortune()
+
+
+
+
